d3e93ac6dc71dfae40392d3890da1764a5cfa84a
Wayne Collier <ke4tri@gmail.com>
2019-03-15 13:25:18 -0500

    have some valid stuff pringing out, need to make it specific to instructions now that working

--- ImagineDragons/Program.cs
+++ ImagineDragons/Program.cs
@@ -18,11 +18,41 @@ namespace ImagineDragons
             var songArtist = song.Artist;
             var songName = song.Name;
 
+            Console.WriteLine($"This is what you chose {songArtist}, and {songName}");
            
             
-            Console.WriteLine($"This is what you chose {songArtist}, and {songName}");
-            song.SaveToDictionary(songArtist, songName);
+
+            // How do you set this to empty:
+            var songListArtistOne = new List<string> { "Good Dude","Love Man" };
+            // HARDCODE ADDING SONGS TO songString like below
+            songListArtistOne.Add("Yummy");
+            songListArtistOne.Add("Newer Song");
             
+            // CREATE A DICTIONARY FOR THE ARTIST THAT TAKS PARAMS OF ARTIST
+            // AND THEN THE LIST OF THE ARTIST SONGS CREATED ABOVE
+            // How do you set this to empty?
+            var newSongDictionary = new Dictionary<string, List<string>>
+            {
+                { "Sammple Artist", songListArtistOne}
+            };
+
+            // ADDING A NEW ARTIST AND THE ARTIS LIST OF SONGS
+            newSongDictionary.Add("Post Malone", new List<string> { "It was a good year song" });
+            // OR
+            // newSongDictionary.Add("Post Malone", new List<string>());
+
+            foreach (var(key, value) in newSongDictionary)
+            {
+                Console.WriteLine($"This is just something to console {key}");
+
+                foreach (var songIn in value)
+                {
+                    Console.WriteLine(songIn);
+                }
+            }
+
+            Console.WriteLine();
+
             Console.ReadLine();
         }
 
@@ -41,14 +71,14 @@ namespace ImagineDragons
 
             //List<string> goodSongs = new List<string>();
             //List<string> allSongs = new List<string>();
-            IDictionary<string, string> goodSongs = new Dictionary<string, string>();
-            IDictionary<string, string> allSongs = new Dictionary<string, string>();
+            //IDictionary<string, string> goodSongs = new Dictionary<string, string>();
+            //IDictionary<string, string> allSongs = new Dictionary<string, string>();
 
             
-            public void SaveToDictionary(string name, string song)
-            {
-                goodSongs.Add(name, song);
-            }
+            //public void SaveToDictionary(string name, string song)
+            //{
+            //    goodSongs.Add(name, song);
+            //}
         }
     }
 }
