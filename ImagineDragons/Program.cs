e8ff2cb2303467e28e30210b2bde22eb9b075608
Wayne Collier <ke4tri@gmail.com>
2019-03-15 06:59:53 -0500

    first commit

--- ImagineDragons/Program.cs
+++ ImagineDragons/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ImagineDragons
 {
@@ -6,7 +7,48 @@ namespace ImagineDragons
     {
         static void Main(string[] args)
         {
+
+            
+            
+
+
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("We are going to add some songs");
+            Console.WriteLine("Name an artist : ");
+            Console.ReadLine();
+            var userArtistInput = Console.ReadLine();
+            Console.WriteLine("Name a song : ");
+           
+            var userNameInput = Console.ReadLine();
+            var song = new Song(userArtistInput, userNameInput);
+
+
+            Console.WriteLine($"This is what you chose {song}");
+
+            Console.ReadLine();
+
+
+        }
+
+        public class Song
+        {
+            // not sure this does anything to make it "required"
+            public string Artist { get; set; }
+            public string Name { get; set; }
+
+            
+            // Constructor
+            public Song(string artist, string name)
+            {
+                Artist = artist;
+                Name = name;
+            }
+
+            List<string> goodSongs = new List<string>();
         }
     }
 }
+
+
+            
