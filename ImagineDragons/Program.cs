5894f26dcd996fff3d3381266eedb30683965c4f
Wayne Collier <ke4tri@gmail.com>
2019-03-15 10:39:18 -0500

    constructed dictionary

--- ImagineDragons/Program.cs
+++ ImagineDragons/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ImagineDragons
 {
@@ -10,12 +11,16 @@ namespace ImagineDragons
 
             Console.WriteLine("What is a name: ");
             var nameChose = Console.ReadLine();
+            Console.WriteLine("What is a song: ");
+            var songChose = Console.ReadLine();
             
-            var song = new Song(nameChose, "smith");
-            var song2 = song.Artist;
+            var song = new Song(nameChose, songChose);
+            var songArtist = song.Artist;
+            var songName = song.Name;
 
-            Console.WriteLine($"This is what you chose {song2}");
-
+            
+            Console.WriteLine($"This is what you chose {songArtist}, and {songName}");
+            song.SaveToDictionary(songArtist, songName);
             Console.ReadLine();
         }
 
@@ -32,11 +37,20 @@ namespace ImagineDragons
                 Name = name;
             }
 
-            List<string> goodSongs = new List<string>();
-            List<string> allSongs = new List<string>();
+            //List<string> goodSongs = new List<string>();
+            //List<string> allSongs = new List<string>();
+            IDictionary<string, string> goodSongs = new Dictionary<string, string>();
+            IDictionary<string, string> allSongs = new Dictionary<string, string>();
+
+            public void SaveToDictionary(string name, string song)
+            {
+                goodSongs.Add(name, song);
+            }
         }
     }
 }
+           
+
             
 
 
