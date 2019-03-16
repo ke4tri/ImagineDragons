5396405a6283714ae706bdc7fd60579fb814ac02
Wayne Collier <ke4tri@gmail.com>
2019-03-16 09:40:32 -0500

    reorganized code and added some to the dictonary

--- ImagineDragons/Program.cs
+++ ImagineDragons/Program.cs
@@ -9,56 +9,73 @@ namespace ImagineDragons
         static void Main(string[] args)
         {
 
-            Console.WriteLine("What is a name: ");
+            Console.WriteLine("What is the name of an artist you like? : ");
             var nameChose = Console.ReadLine();
-            Console.WriteLine("What is a song: ");
+            Console.WriteLine("What is a song of theres to add to a list? : ");
             var songChose = Console.ReadLine();
-            
+
+            ////////////////////////////////////// List /////////////////////////
+            IList<string> postMalone = new List<string>();
+            postMalone.Add("Better Now");
+            postMalone.Add("Congradulations");
+
+            IList<string> eltonJohn = new List<string>();
+            postMalone.Add("Tiny Dancer");
+            postMalone.Add("Yellow Brick Road");
+
+            List<string> artistSongLists = new List<string>();
+            artistSongLists.AddRange(postMalone);
+            artistSongLists.AddRange(eltonJohn);
+
+            /////////////////////////////////////////////////////////////////////
+           
             var song = new Song(nameChose, songChose);
             var songArtist = song.Artist;
             var songName = song.Name;
 
-            Console.WriteLine($"This is what you chose {songArtist}, and {songName}");
-           
-            
+            //Console.WriteLine($"This is what you chose {songArtist}, and {songName}");
 
-            // How do you set this to empty:
-            var songListArtistOne = new List<string> { "Good Dude","Love Man" };
-            // HARDCODE ADDING SONGS TO songString like below
-            songListArtistOne.Add("Yummy");
-            songListArtistOne.Add("Newer Song");
-            
-            // CREATE A DICTIONARY FOR THE ARTIST THAT TAKS PARAMS OF ARTIST
-            // AND THEN THE LIST OF THE ARTIST SONGS CREATED ABOVE
+            //////////////////////////////// List of songs for indeivdual artis///
+            var songListSample = new List<string> { "Sample This Song", "This one Too" };
+            songListSample.Add("For good messure");
+            songListSample.Add("Here is another one");
+            var songListPost = new List<string> { "Good Dude","Love Man" };
+            songListPost.Add("Yummy");
+            songListPost.Add("Newer Song");
+            var songListWayne = new List<string> { "One for the Mony", "Love That Woman" };
+            songListWayne.Add("One more for the road Dude");
+
             // How do you set this to empty?
             var newSongDictionary = new Dictionary<string, List<string>>
             {
-                { "Sammple Artist", songListArtistOne}
+                //hardcoded this artist
+                { "Sammple Artist", songListSample}
             };
 
-            // ADDING A NEW ARTIST AND THE ARTIS LIST OF SONGS
-            newSongDictionary.Add("Post Malone", new List<string> { "It was a good year song" });
-            // OR
-            // newSongDictionary.Add("Post Malone", new List<string>());
+            // Below adds the songlst to the above dictionary along with the name of artist
+            // Commented the newSongDictionayr.Add below out becuase it was already hardcoded in
+            // side the dictionary 
+            // newSongDictionary.Add("Sample Artist", songListSample);
+            newSongDictionary.Add("Wayne Collier", songListWayne);
+            newSongDictionary.Add("Post Malone", songListPost);
 
             foreach (var(key, value) in newSongDictionary)
             {
-                Console.WriteLine($"This is just something to console {key}");
+                Console.WriteLine(" ");
+                Console.WriteLine($"This should be an artist name {key} ");
 
                 foreach (var songIn in value)
                 {
-                    Console.WriteLine(songIn);
+                    Console.WriteLine($"This should be a song {songIn}");
                 }
             }
 
             Console.WriteLine();
-
             Console.ReadLine();
         }
 
         public class Song
         {
-           
             public string Artist { get; set; }
             public string Name { get; set; }
 
@@ -68,20 +85,33 @@ namespace ImagineDragons
                 Artist = artist;
                 Name = name;
             }
-
-            //List<string> goodSongs = new List<string>();
-            //List<string> allSongs = new List<string>();
-            //IDictionary<string, string> goodSongs = new Dictionary<string, string>();
-            //IDictionary<string, string> allSongs = new Dictionary<string, string>();
-
-            
-            //public void SaveToDictionary(string name, string song)
-            //{
-            //    goodSongs.Add(name, song);
-            //}
         }
+
+        //public class AddList
+        //{
+        //    public string Artist { get; set; }
+        //    public string Name { get; set; }
+
+        //    // Constructor
+        //    public AddList(string artist, string song)
+        //    {
+        //        artist.Add(song);
+        //    }
+        //}
     }
 }
+
+
+
+
+
+
+
+
+
+           
+
+            
            
 
             
